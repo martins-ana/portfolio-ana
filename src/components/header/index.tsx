07a854577d43a6bdbf7f63184df13f403fc6f69d
Ana Paula <martinsgarciaana@outlook.com>
2024-11-05 10:57:51 -0300

    feat: menu de rede sociais, botões da página inicial e phosphor icons, todos estilizados e finalizados

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,10 +5,18 @@ import { PAGES } from '../../constants';
 export default function Header() {
   return (
     <nav className='navigation-menu'>
-      <Link to={PAGES.HOME}>Home</Link>
-      <Link to={PAGES.ABOUT}>About Me</Link>
-      <Link to={PAGES.PROJECTS}>Projects</Link>
-      <Link to={PAGES.CONTACT}>Contact</Link>
+      <Link to={PAGES.HOME} className='header-buttons'>
+        Home
+      </Link>
+      <Link to={PAGES.ABOUT} className='header-buttons'>
+        About Me
+      </Link>
+      <Link to={PAGES.PROJECTS} className='header-buttons'>
+        Projects
+      </Link>
+      <Link to={PAGES.CONTACT} className='header-buttons'>
+        Contact
+      </Link>
     </nav>
   );
 }
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,35 +1,60 @@
 import { Link } from 'react-router-dom';
 import { PAGES } from '../../constants';
 import './style.css';
+import { EnvelopeSimple, GithubLogo, LinkedinLogo } from '@phosphor-icons/react';
 
 export default function HomeInitial() {
   return (
-    <section>
-      <div className='home-initial-presentation'>
+    <>
+      <section className='home-initial-presentation'>
         <img
           className='home-initial-portrait'
           src='/images/ana-destaque.png'
           alt='Portrait of a young woman with long dark hair, smiling softly with one hand resting on her chin. The image has an irregular white outline on a transparent background, highlighting the womans face.'
         />
-        <div className='home-initial-card-description'>
-          <h1 className='home-initial-title'>Hi, I am</h1>
-          <h2 className='home-initial-subtitle'>Ana Paula!</h2>
-          <h2 className='home-initial-subtitle-2'>Front-end Developer</h2>
-          <p className='home-initial-description'>
-            I am passionate about creating modern and authentic websites that not only
-            attract visually but also provide unique, easy, and intuitive experiences.
-          </p>
+        <div className='home-initial-card-and-buttons'>
+          <div className='home-initial-card-description'>
+            <h1 className='home-initial-title'>Hi, I am</h1>
+            <h2 className='home-initial-subtitle'>Ana Paula!</h2>
+            <h2 className='home-initial-subtitle-2'>Front-end Developer</h2>
+            <p className='home-initial-description'>
+              I am passionate about creating modern and authentic websites that not only
+              attract visually but also provide unique, easy, and intuitive experiences.
+            </p>
+          </div>
+          <div className='home-buttons'>
+            <Link className='button-download-and-get-in-touch' to=''>
+              Download CV
+            </Link>
+            <Link className='button-download-and-get-in-touch' to={PAGES.CONTACT}>
+              Get in Touch!
+            </Link>
+          </div>
         </div>
+      </section>
+      <div className='home-social-medias'>
+        <Link
+          className='buttons-social-medias'
+          to='mailto:martinsgarciaana@outlook.com'
+          target='_blank'
+        >
+          <EnvelopeSimple size={24} />
+        </Link>
+        <Link
+          className='buttons-social-medias'
+          to='https://github.com/martins-ana'
+          target='_blank'
+        >
+          <GithubLogo size={24} />
+        </Link>
+        <Link
+          className='buttons-social-medias'
+          to='https://www.linkedin.com/in/ana-paulamartins/'
+          target='_blank'
+        >
+          <LinkedinLogo size={24} />
+        </Link>
       </div>
-      <div>
-        <Link to=''>Download CV</Link>
-        <Link to={PAGES.CONTACT}>Get in Touch!</Link>
-      </div>
-      <div>
-        <Link to=''>Download CV</Link>
-        <Link to=''>Get in Touch!</Link>
-        <Link to=''>Get in Touch!</Link>
-      </div>
-    </section>
+    </>
   );
 }
